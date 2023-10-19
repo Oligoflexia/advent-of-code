13e78f4143c67688443dd91d72955143a649cefc
Souvik Maiti <souvik.maiti1@ucalgary.ca>
2023-10-19 16:00:50 -0600

    Started implementing 2015:7 pt.1

--- 2015/7.py
+++ 2015/7.py
@@ -2,4 +2,25 @@ from utils.get_input import get_input
 
 input = get_input(2015, 7)
 
-print(input)
+class Node:
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+        self.next = []
+        self.prev = None
+    
+    def set_next(self, Node):
+        self.next.append(Node)
+        return
+    
+    def get_next(self):
+        return self.next
+    
+    def set_prev(self, Node):
+        self.prev = Node
+        return
+    
+    def get_prev(self):
+        return self.prev
+
+
