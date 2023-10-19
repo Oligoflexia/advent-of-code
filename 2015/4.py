144c18692e384287ba5b9b1b763592299aba139e
Souvik Maiti <souvik.maiti1@ucalgary.ca>
2023-10-19 12:51:29 -0600

    fixed errors because /n insertion in .txt creation

--- 2015/4.py
+++ 2015/4.py
@@ -1,30 +1,32 @@
 import hashlib
+from utils.get_input import get_input
+
+input = get_input(2015, 4)
 
 test = 'abcdef609043'
 test1 = 'pqrstuv1048970'
 
-input = 'yzbqklnj'
-
 def generate_md5(input):
-    
     hash = hashlib.md5()
-    
     hash.update(input.encode('utf-8'))
-    
     return hash.hexdigest()
 
 
-def brute_force(input):
+def brute_force(input, zeros):
     n = 1
     
     while True:
         hash = generate_md5(input + str(n))
 
-        if hash[:6] == '000000':
-            print(n)
-            break
+        if hash[:zeros] == '0' * zeros:
+            print(f"The smallest possible integer is {n}.")
+            return str(n)
         n+=1
 
 
-#brute_force(input)
-print(generate_md5(input + str(9962624)))
+five = brute_force(input, 5)
+print(f"The generated hash is {generate_md5(input + five)}.")
+
+six = brute_force(input, 6)
+print(f"The generated hash is {generate_md5(input + six)}.")
+
