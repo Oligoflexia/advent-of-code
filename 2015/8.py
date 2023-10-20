d6eb0930a70a1d29f241374fdce0b3b8d9794ea3
Souvik Maiti <smaiti@alumni.ubc.ca>
2023-10-20 02:05:02 -0600

    started on 2015:8 pt.1

--- 2015/8.py
+++ 2015/8.py
@@ -1,3 +1,31 @@
 from utils.get_input import get_input
 
 input = get_input(2015, 8)
+
+for line in input.splitlines():
+    literal = len(line)
+    
+    total = 0
+    
+    for index, character in enumerate(line):
+        if character == '\\':
+            if line[index + 1] == '\\':
+                pre = line[:index]
+                post = line[index + 2]
+                new = pre + ' ' +post
+                print(len(new))
+                total += (literal - len(new))
+            elif line[index + 1] == "\"":
+                pre = line[:index]
+                post = line[index + 2]
+                new = pre + ' ' + post
+                print(len(new))
+                total += (literal - len(new))
+            else:
+                pre = line[:index]
+                post = line[index + 4]
+                new = pre + ' ' +post
+                print(len(new))
+                total += (literal - len(new))
+
+            
