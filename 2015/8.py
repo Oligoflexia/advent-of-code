431851ce2d4a44dbd5c2812e305122d6cbf2bd6e
Souvik Maiti <souvik.maiti1@ucalgary.ca>
2023-10-20 08:41:01 -0600

    working on 2015:8 pt.1

--- 2015/8.py
+++ 2015/8.py
@@ -2,30 +2,29 @@ from utils.get_input import get_input
 
 input = get_input(2015, 8)
 
+literal = 0
+mem = 0
+modified = []
+
 for line in input.splitlines():
-    literal = len(line)
+    literal += len(line)
     
-    total = 0
-    
-    for index, character in enumerate(line):
+    #running into issues becuase iterating over a modified string. Need to work on a copy. 
+    for index, character in enumerate(line[:-1]):
         if character == '\\':
             if line[index + 1] == '\\':
-                pre = line[:index]
-                post = line[index + 2]
-                new = pre + ' ' +post
-                print(len(new))
-                total += (literal - len(new))
-            elif line[index + 1] == "\"":
-                pre = line[:index]
-                post = line[index + 2]
-                new = pre + ' ' + post
-                print(len(new))
-                total += (literal - len(new))
-            else:
-                pre = line[:index]
-                post = line[index + 4]
-                new = pre + ' ' +post
-                print(len(new))
-                total += (literal - len(new))
+                line = line.replace('\\\\', '$', 1)
+                print(line)
+            elif line[index + 1] == "\\\"":
+                line = line.replace('\\\"', '$', 1)
+                print(line)
+            elif line[index + 1] == "x":
+                #convert - think about this one
+                pattern = f"\\x{line[index+2]}{line[index+3]}"
+                line = line.replace(pattern, '$', 1)
+                print(line)
 
+
+         
+        
             
