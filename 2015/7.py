b8f63bd8c2a9c168b1ea6b40d2d52477a4fb1704
Souvik Maiti <smaiti@alumni.ubc.ca>
2023-10-19 18:39:45 -0600

    setting up a tree based approach

--- 2015/7.py
+++ 2015/7.py
@@ -3,24 +3,12 @@ from utils.get_input import get_input
 input = get_input(2015, 7)
 
 class Node:
-    def __init__(self, name, data):
-        self.name = name
-        self.data = data
-        self.next = []
-        self.prev = None
-    
-    def set_next(self, Node):
-        self.next.append(Node)
-        return
-    
-    def get_next(self):
-        return self.next
-    
-    def set_prev(self, Node):
-        self.prev = Node
-        return
-    
-    def get_prev(self):
-        return self.prev
+    def __init__(self):
+        pass
+
+class Edge:
+    def __init__(self):
+        pass
+
 
 
