5a7a786482f971ee9e24700c9b50de1d4992ac16
Souvik Maiti <smaiti@alumni.ubc.ca>
2023-10-25 23:24:25 -0600

    started solving 2015:12 pt.2

---
+++ 2015/12.py
@@ -0,0 +1,24 @@
+import re
+from utils.get_input import get_input
+
+input = get_input(2015, 12)
+
+def str_sum(s:str) -> int:
+    pattern = r'-?\d+'
+    nums = re.findall(pattern, s)
+    
+    return sum(int(x) for x in nums)
+
+print(f"The sum of all the integers in the input is: {str_sum(input)}")
+
+def no_red(s:str) -> int:
+    pattern = r'{.*?red.*?}'
+    
+    new = re.sub(pattern, "", s)
+    
+    print(new)
+    
+    return str_sum(new)
+
+print(f"The sum of all the integers in the input is: {no_red(input)}")
+
