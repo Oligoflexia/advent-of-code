e488e4624541ebfa401f34743f0a42cd7fb4ad0b
Souvik Maiti <smaiti@alumni.ubc.ca>
2023-11-05 16:07:32 -0700

    solved 2015:pt.1 and 2

--- 2015/20.py
+++ 2015/20.py
@@ -1,47 +1,25 @@
 from utils.get_input import get_input
-from math import gcd
-from itertools import product
 
 input = int(get_input(2015, 20))
 
-def primes_lte(n:int) -> list[int]:
-    out = list([2])
-    sieve = [True] * (n+1)
-    for p in range(3, n+1, 2):
-        if sieve[p]:
-            out.append(p)
-            for i in range(p*p, n+1, p):
-                sieve[i] = False
-    return out
+def lowest_house(bound:int) -> int:
+    houses = [0] * (bound // 10)
+    for elf in range(1, len(houses)):
+        for house in range(elf, len(houses), elf):
+            houses[house] += elf * 10
+        if houses[elf] >= bound:
+            return elf
 
-def prime_factorization(n:int, primes:list[int]) -> dict[int:int]:
-    factors = {}
-    for prime in primes:
-        while n % prime == 0:
-            factors[prime] = factors.get(prime, 0) + 1
-            n //= prime
-        if n == 1:
-            break
-    if n != 1:
-        factors[n] = 1
-    return factors
-
-def lowest_house(bound:int, primes:set[int]) -> int:
-    def sum_factors(prime_factors):
-        total = 1
-        for prime, exponent in prime_factors.items():
-            total *= (prime**(exponent + 1) - 1) // (prime - 1)
-        return total
-    
-    i = 1
-    while True:
-        prime_factors = prime_factorization(i, primes)
-        total = sum_factors(prime_factors) * 10
-        if total >= bound:
-            return i
-        i += 1
-
-primes = primes_lte(int(input ** 0.5))
-print(lowest_house(input, primes))
+print(lowest_house(input))
 
 # add a new bound which checks if factor * 50 is less than the current number
+def lowest_house_restricted(bound:int) -> int:
+    houses = [0] * (bound // 10)
+    for elf in range(1, len(houses)):
+        for house in range(elf, min(elf * 50, len(houses)), elf):
+            houses[house] += elf * 11
+        if houses[elf] >= bound:
+            return elf
+
+
+print(lowest_house_restricted(input))
