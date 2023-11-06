d6a947d5df1c46a44e624a96aa4d2179b3d59dac
Souvik Maiti <souvik.maiti1@ucalgary.ca>
2023-11-06 16:14:41 -0700

    continue building dataclasses wizard/boss

--- 2015/22.py
+++ 2015/22.py
@@ -14,42 +14,58 @@ class Entity:
     def defeated(self) -> bool:
         return self.health <= 0
 
-    def take_dmg(self, dmg:int, stat:bool, *args):
+    def take_dmg(self, dmg:int, stat:bool = False, *args):
+        if stat:
+            self.status.append(args)
         if self.armour >= dmg:
             self.health -= 1
         else:
             self.health -= dmg - self.armour   
-        if stat:
-            self.status.append(arg)
                     
     def attack(self) -> int:
         return self.damage
+    
+    def apply_ticks(self):
+        for item in self.status:
+            for condition, turns in item:
+                if turns == 0:
+                    self.status.remove(item)
+                match condition:
+                    case 
+                
 
 @dataclass
 class Wizard(Entity):
     mana: int = 500
     health: int = 50
     
-    def oom(self):
-        return mana < 53
+    def oom(self, n:int):
+        return self.mana < int
     
     def cast_missile(self):
-        if not oom():
+        if not self.oom(53):
             self.mana -= 53
             return 4
         
     def cast_drain(self):
-        if not oom():
+        if not self.oom(73):
             self.mana -= 73
             self.health += 2
             return 2
     
-    def cast_shield():
-        pass
+    def cast_shield(self):
+        if not self.oom(113):
+            self.mana -= 113
+            self.status.append(('Shield', 6))
+            return 0
+            
+    def cast_poison(self):
+        if not self.oom(173):
+            self.mana -= 173
+            return 0, True, ('Poison', 6)
     
-    def cast_poison():
-        pass
-    
-    def cast_recharge():
-        pass
-    
+    def cast_recharge(self):
+        if not self.oom(229):
+            self.mana -= 229
+            self.status.append(('Recharge', 5))
+            return 0
