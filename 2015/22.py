92b31e38da67e46bc2685ab64e7882ea5670e5ca
Souvik Maiti <smaiti@alumni.ubc.ca>
2023-11-06 22:06:18 -0700

    continued building out classes

--- 2015/22.py
+++ 2015/22.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from queue import SimpleQueue
 from utils.get_input import get_input
 
 input = get_input(2015, 22)
@@ -14,58 +15,113 @@ class Entity:
     def defeated(self) -> bool:
         return self.health <= 0
 
-    def take_dmg(self, dmg:int, stat:bool = False, *args):
-        if stat:
-            self.status.append(args)
-        if self.armour >= dmg:
+    def take_dmg(self, args:tuple):
+        if args[1]:
+            self.status.append(args[2])
+        elif self.armour >= args[0]:
             self.health -= 1
         else:
-            self.health -= dmg - self.armour   
+            self.health -= args[0] - self.armour   
                     
     def attack(self) -> int:
-        return self.damage
+        return self.damage, False, None
     
     def apply_ticks(self):
         for item in self.status:
-            for condition, turns in item:
+            for condition, turns in item.items():
                 if turns == 0:
                     self.status.remove(item)
+                    if condition == 'Shield':
+                        self.armour = 0
+                    break
                 match condition:
-                    case 
-                
+                    case 'Shield':
+                        self.armour = 7
+                    case 'Poison':
+                        self.health -= 3
+                    case 'Recharge':
+                        self.mana += 110
+                item[condition] -= 1
 
 @dataclass
 class Wizard(Entity):
     mana: int = 500
     health: int = 50
+    spent_mana: int = 0
     
-    def oom(self, n:int):
-        return self.mana < int
+    def oom(self):
+        return self.mana < 53
     
     def cast_missile(self):
-        if not self.oom(53):
+        if not self.oom():
             self.mana -= 53
-            return 4
+            self.spent_mana += 53
+            return 4, False, None
         
     def cast_drain(self):
-        if not self.oom(73):
+        if not self.oom():
             self.mana -= 73
             self.health += 2
-            return 2
+            self.spent_mana += 73
+            return 2, False, None
     
     def cast_shield(self):
-        if not self.oom(113):
+        if not self.oom():
             self.mana -= 113
-            self.status.append(('Shield', 6))
-            return 0
+            self.spent_mana += 113
+            self.status.append({'Shield': 6})
+            return 0, False, None
             
     def cast_poison(self):
-        if not self.oom(173):
+        if not self.oom():
             self.mana -= 173
-            return 0, True, ('Poison', 6)
+            self.spent_mana += 173
+            return 0, True, {'Poison': 6}
     
     def cast_recharge(self):
-        if not self.oom(229):
+        if not self.oom():
             self.mana -= 229
-            self.status.append(('Recharge', 5))
-            return 0
+            self.spent_mana += 229
+            self.status.append({'Recharge': 5})
+            return 0, False, None
+
+player = Wizard(name= 'Player')
+boss = Entity(name= 'Boss', health= 58, damage= 9)
+
+while not player.defeated() and not player.oom():
+    boss.apply_ticks()
+    player.apply_ticks()
+    if boss.defeated():
+        print("You win!")
+        break
+    boss.take_dmg(player.cast_poison())
+    if boss.defeated():
+        print("You win!")
+        break
+    boss.apply_ticks()
+    player.apply_ticks()
+    if boss.defeated():
+        print("You win!")
+        break
+    player.take_dmg(boss.attack())
+    
+def simulator(q:SimpleQueue, p:Wizard, b:Entity):
+    while not q.empty():
+        while not player.defeated() and not player.oom():
+            player.apply_ticks()
+            boss.apply_ticks()
+            if boss.defeated():
+                print("You win!")
+                break
+            boss.take_dmg(q.get())
+            if boss.defeated():
+                print("You win!")
+                break
+            boss.apply_ticks()
+            player.apply_ticks()
+            if boss.defeated():
+                print("You win!")
+                break
+            player.take_dmg(boss.attack())
+
+
