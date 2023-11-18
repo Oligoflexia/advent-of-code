029e239feed956c5183a0f37f9a6465a9cd30cf6
Souvik Maiti <smaiti@alumni.ubc.ca>
2023-11-17 19:48:39 -0700

    solved 2016:5 pt. 1 and 2 (slow)

---
+++ 2016/5.py
@@ -0,0 +1,34 @@
+import hashlib
+from utils import helpers
+
+input = helpers.get_input(2016, 5)
+
+def hash_finder(start_str:str):
+    password = []
+    i = 0
+    while len(password) < 8:
+        cur_str = start_str + str(i)
+        hex_hash = hashlib.md5(cur_str.encode()).hexdigest()
+        if hex_hash[:5] == "00000":
+            password.append(str(hex_hash[5]))
+        i += 1
+    return "".join(password)
+
+print(hash_finder(input))
+
+def complex_hash_finder(start_str:str):
+    password = {0: None, 1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None}
+    i = 0
+    n = 0
+    while n < 8:
+        cur_str = start_str + str(i)
+        hex_hash = hashlib.md5(cur_str.encode()).hexdigest()
+        if hex_hash[:5] == "00000" and hex_hash[5].isdigit():
+            if int(hex_hash[5]) in password:
+                if password[int(hex_hash[5])] is None:
+                    password[int(hex_hash[5])] = str(hex_hash[6])
+                    n += 1
+        i += 1
+    return "".join(password.values())
+
+print(complex_hash_finder(input))
