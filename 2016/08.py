19f4d793a0783d2f83194d61d4d1db7545e31dd2
Souvik Maiti <souvik.maiti1@ucalgary.ca>
2023-11-27 16:02:44 -0700

    implemented rect and row rotate helpers

--- 2016/08.py
+++ 2016/08.py
@@ -2,7 +2,7 @@ from utils import helpers
 
 input = helpers.get_input(2016, 8)
 
-grid = [[0] * 50 for _ in range(6)]
+grid = [0 for _ in range(50*6)]
 
 steps = []
 
@@ -22,6 +22,26 @@ for step in steps:
         a, b = step[0].split("=")
         instructions.append([a, b, step[1]])
 
-# for i in instructions:
-#     print(i)
+def rect(w:int, h:int, grd:list[int]) -> list[int]:
+    for row in range(h):
+        for col in range(w):
+            grd[(row*50)+col] = 1
+    
+    return grd
+
+def row_rotate(row:int, shift:int, grd:list[int]) -> list[int]:
+    grd_cpy = grd[row*50:(row*50)+50].copy()
+     
+    for i, element in enumerate(grd_cpy):
+        if element == 1:
+            grd[row*50+i] = 0
+            grd[row*50+((i+shift)%50)] = 1
+    
+    return grd
+
+
+    
+
+    
+    
 
